7ae9840a5b98f2e2cdb60912c3f56564f4c153a1
Miracle Shaibu <shaibumiracle7@gmail.com>
2024-05-21 14:07:47 +0100

    worked on yaml

---
+++ businessManagement/src/main/java/com/project/businessManagement/BusinessManagementApplication.java
@@ -0,0 +1,13 @@
+package com.project.businessManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusinessManagementApplication.class, args);
+	}
+
+}
---
+++ businessManagement/src/test/java/com/project/businessManagement/BusinessManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.businessManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusinessManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
